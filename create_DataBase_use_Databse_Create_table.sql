cf6859b965abbb7577a1fae691d0924c95e1e25f
Chaudhary-Devesh <chaudharydevesh704@gmail.com>
2025-07-26 15:53:06 +0530

    first commit

---
+++ create_DataBase_use_Databse_Create_table.sql
@@ -0,0 +1,13 @@
+create database startersql;
+use startersql;
+
+create table users(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+email VARCHAR(100) UNIQUE NOT NULL,
+gender ENUM("Male","Female","Other"),
+date_of_birth DATE,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+SELECT * FROM users;
